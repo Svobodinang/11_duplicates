73ee80dabc8b857ac59f62e11b10e1709a4ba2b9
svobodinang <svobodinang@mail.ru>
2018-06-10 22:33:49 +0300

    optimized the code

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ for works this script you must to do (You must specify the path to the folder)
 ```bash python
 C:\Users\svobodinang\Desktop\Python\8_vk_friends_online>python duplicates.py C:\Users\svobodinang\Pictures\test
 Дубликаты:
-['1.txt']
+1.txt Пути:  [['C:\\Users\\svobodinang\\Pictures\\test', 'C:\\Users\\svobodinang\\Pictures\\test\\test2']]
 ```
 
 # Project Goals
--- duplicates.py
+++ duplicates.py
@@ -3,35 +3,37 @@ import sys
 import collections
 
 
-def find_all_files(filepath):
+def find_all_files_in_filepath(filepath):
     tree = os.walk(filepath)
-    files = []
-    for argumen in tree:
-        for file_name in argumen[2]:
-            files.append(file_name)
-    return files
+    paths_to_files = collections.defaultdict(list)
+    for dir_path, _, file_names in tree:
+        for file_name in file_names:
+            paths_to_files[file_name].append(dir_path)
+    return paths_to_files
 
 
-def find_dublicates(files):
-    dublicates = []
-    count_mas = collections.Counter()
-    for file_name in files:
-        count_mas[file_name] += 1
-    for file_name in files:
-        if (count_mas[file_name] > 1) and (file_name not in dublicates):
-            dublicates.append(file_name)
+def find_dublicates(paths_to_files):
+    dublicates = collections.defaultdict(list)
+    path = 1
+    name = 0
+    for name in paths_to_files:
+        if len(paths_to_files[name]) > 1:
+            dublicates[name].append(paths_to_files[name])
     return dublicates
 
 
 def print_dublicates(dublicates):
     print('Дубликаты: ')
-    print(dublicates)
+    for name in dublicates:
+        print(name, 'Пути: ', dublicates[name])
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         exit('Вы не ввели путь к папке')
     filepath = sys.argv[1]
-    files = find_all_files(filepath)
-    dublicates = find_dublicates(files)
+    if os.path.isdir(filepath) is False:
+        exit('Данный путь не ведет к каталогу')
+    file_name_path = find_all_files_in_filepath(filepath)
+    dublicates = find_dublicates(file_name_path)
     print_dublicates(dublicates)
